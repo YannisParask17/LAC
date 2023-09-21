36b50107562f5c94d83824b9df70cd2536eca0a8
mjanszen <mijanszen@gmail.com>
2023-09-21 09:59:01 +0200

    Plot cl cd and aoa

--- src/plot_cl_cd_design_function.py
+++ src/plot_cl_cd_design_function.py
@@ -1,45 +1,59 @@
 # Script to plot Cl, Cd and AOA
 
 # -------------------------------------#
-# IMports 
+# Imports
 # -------------------------------------#
 
 import numpy as np
 import json
 import matplotlib.pyplot as plt
 
+from aero_design_functions import get_design_functions
+
+cl_des1, cd_des1, aoa_des1, tc_vals1, cl_vals1, cd_vals1, aoa_vals1 = get_design_functions(1)
+cl_des2, cd_des2, aoa_des2, tc_vals2, cl_vals2, cd_vals2, aoa_vals2 = get_design_functions(2)
+cl_des3, cd_des3, aoa_des3, tc_vals3, cl_vals3, cd_vals3, aoa_vals3 = get_design_functions(3)
 
 json_path = "../results/polar_data.json"
-# Opening JSON file
-f = open(json_path)
 
-breakpoint()
-# returns JSON object as
-# a dictionary
+# Pull data from file
+f = open(json_path)
 data = json.load(f)  # a list of dictionaries, list contains all airfoils 1 to 6
-     
-breakpoint()
-# Closing file
 f.close()
 
-tc_list = np.empty([6, 0])  # enter all the thicknesses
-cl = np.empty([6, 0])
-cd = np.empty([6, 0])
-aoa = np.empty([6, 0])
+# initialize arrays
+tc_list = np.zeros([6, 1])  # enter all the thicknesses
+cl = np.zeros([6, 1])
+cd = np.zeros([6, 1])
+aoa = np.zeros([6, 1])
 
 # Get out the Cl, Cd, AOA and thickness for each airfoil -> Design point
-breakpoint()
 for i in range(0, 6):
-    tc_list[i] = data[i]['tc']
-    cl[i] = data[i]['cl_des']
-    cd[i] = data[i]['cd_des']
-    aoa[i] = data[i]['aoa_des']
-
-
+    try:
+        if i < 5:
+            tc_list[i] = data[i]['tc']
+            cl[i] = data[i]['cl_des']
+            cd[i] = data[i]['cd_des']
+            aoa[i] = data[i]['aoa_des']
+        
+        if i == 5:  # Which is the cylinder .... Dont mess up the order !
+            tc_list[i] = data[i]['tc']
+            cl[i] = data[i]['cl'][0]  # Manually set them
+            cd[i] = data[i]['cd'][0]
+            aoa[i] = data[i]['aoa_des']
+    
+    except:
+        breakpoint()
+        print("Something fishy is going on")
+# Plot
 fig, axs = plt.subplots(3, 1)
-axs[0].plot(tc_list, cl)
-axs[1].plot(tc_list, cd)
-axs[2].plot(tc_list, aoa)
+axs[0].scatter(tc_list, cl)
+axs[1].scatter(tc_list, cd)
+axs[2].scatter(tc_list, aoa)
 
+axs[0].set_ylabel('cl')
+axs[1].set_ylabel('cd')
+axs[2].set_ylabel('aoa')
+axs[2].set_xlabel('T/C')
 plt.show()
 breakpoint()
