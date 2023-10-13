908341ad6d71b65843f5091d407995911a19d0d4
mjanszen <mijanszen@gmail.com>
2023-10-13 10:54:59 +0200

    Updated the htc for HAWC2

--- 02_control_IIIB_scaled_turbine/calc_aero_gain_coef.py
+++ 02_control_IIIB_scaled_turbine/calc_aero_gain_coef.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-file_path = "./hawc2s_files/IIIB_scaled_turbine_flex_C1_omega_005_zeta07_ctrl_tuning.txt"
+file_path = "./hawc2s_files/IIIB_scaled_turbine_flex_C5_omega_001_zeta07_ctrl_tuning.txt"
 
 results = np.loadtxt(file_path, skiprows=17)
 # Define the function you want to fit
@@ -27,11 +27,11 @@ fitted_K1, fitted_K2 = params
 rms = np.sqrt(np.mean((dQA_dtheta - Q(theta, Q0, fitted_K1, fitted_K2))**2))
 
 
-
+print(f"K1:{fitted_K1}\nK2:{fitted_K2}")
 # Print the fitted values
-fig, ax = plt.subplots(figsize=(6,3))
-ax.plot(theta, dQA_dtheta, "rx", label = "HAWC2S Results")
-ax.plot(theta, Q(theta, Q0, fitted_K1, fitted_K2), "b-", label = "Curve Fitting (Least Squares)")
+fig, ax = plt.subplots(figsize=(6, 3))
+ax.plot(theta, dQA_dtheta, "rx", label="HAWC2S Results")
+ax.plot(theta, Q(theta, Q0, fitted_K1, fitted_K2), "b-", label="Curve Fitting (Least Squares)")
 ax.text(0.02, 0.02, f'RMS = {rms:.2f}', fontsize=12, transform=ax.transAxes)
 ax.set_xlabel("Pitch Angle [deg]")
 ax.set_ylabel("Aerodynamic Torque Gain [kNm/deg]")
