e24b44c1d7b3a7ef8233eceb4f0ac241bfc42d2b
mjanszen <mijanszen@gmail.com>
2023-10-15 13:04:38 +0200

    More computations done on the C7 case

--- 02_control_IIIB_scaled_turbine/read_res.py
+++ 02_control_IIIB_scaled_turbine/read_res.py
@@ -8,23 +8,25 @@ fn3 = './res/iiib_c3.hdf5'
 fn4 = './res/iiib_c4.hdf5'
 fn5 = './res/iiib_c5.hdf5'
 fn6 = './res/iiib_c6.hdf5'
-fn7 = './res/iIIB_C7.hdf5'
+fn7 = './res/iiib_c7.hdf5'
 
 # Split into two, as there should be two plots
 f_names1=[fn1, fn2, fn3]
 f_names2=[fn4, fn5, fn6]
+f_names3=[fn1, fn7]
 
 labels_1 = ['C1', 'C2', 'C3']
 labels_2 = ['C4', 'C5', 'C6']
+labels_3 = ['C1', 'C7']
 
 # First plot : C1 to C3
 
 fig1, ax1 = plt.subplots(4, figsize=(8, 5), sharex=True)
-ax1[0].set(ylabel='Wind Speed, Vz [m/s]')
-ax1[1].set(ylabel='Pitch Angle [deg]')
-ax1[2].set(ylabel='Rotor Speed [s]')
-ax1[3].set(ylabel='Power [kW]',
-            xlabel='Time [s]')
+ax1[0].set(ylabel='Wind Speed, Vz\n[m/s]')
+ax1[1].set(ylabel='Pitch Angle\n[deg]')
+ax1[2].set(ylabel='Rotor Speed\n[s]')
+ax1[3].set(ylabel='Power\n[kW]',
+            xlabel='Time\n[s]')
 
 
 for i, fn in enumerate(f_names1):
@@ -46,11 +48,11 @@ plt.tight_layout()
 plt.savefig('../results/control/control_response_c1_c3.pdf')
 
 fig2, ax2 = plt.subplots(4, figsize=(8, 5), sharex=True)
-ax2[0].set(ylabel='Wind Speed, Vz [m/s]')
-ax2[1].set(ylabel='Pitch Angle [deg]')
-ax2[2].set(ylabel='Rotor Speed [s]')
-ax2[3].set(ylabel='Power [kW]',
-           xlabel='Time [s]')
+ax2[0].set(ylabel='Wind Speed, Vz\n[m/s]')
+ax2[1].set(ylabel='Pitch Angle\n[deg]')
+ax2[2].set(ylabel='Rotor Speed\n[s]')
+ax2[3].set(ylabel='Power\n[kW]',
+           xlabel='Time\n[s]')
 
 for i, fn in enumerate(f_names2):
     h2res = ReadHAWC2(fn)
@@ -69,4 +71,31 @@ for ax in ax2.flat:
     ax.grid(True)
 plt.tight_layout()
 plt.savefig('../results/control/control_response_c4_c6.pdf')
+
+fig3, ax3 = plt.subplots(4, figsize=(8, 5), sharex=True)
+ax3[0].set(ylabel='Wind Speed, Vz\n[m/s]')
+ax3[1].set(ylabel='Pitch Angle\n[deg]')
+ax3[2].set(ylabel='Rotor Speed\n[s]')
+ax3[3].set(ylabel='Power\n[kW]',
+           xlabel='Time\n[s]')
+
+
+for i, fn in enumerate(f_names3):
+    h2res = ReadHAWC2(fn)
+
+    print(h2res.__dict__.keys())
+    names, units, desc = h2res.chaninfo
+
+    # plot the time series
+    ax3[0].plot(h2res.t, h2res.data[:, 14], label=labels_3[i])
+    ax3[1].plot(h2res.t, h2res.data[:, 3], label=labels_3[i])
+    ax3[2].plot(h2res.t, h2res.data[:, 9], label=labels_3[i])
+    ax3[3].plot(h2res.t, h2res.data[:, 110], label=labels_3[i])
+
+ax3[0].legend(loc='upper center', ncols=3)
+for ax in ax3.flat:
+    ax.grid(True)
+
+plt.tight_layout()
+plt.savefig('../results/control/control_response_c1_c7.pdf')
 plt.show()
